b1006f753eb29858cfb9cfe5c5ba5c8170f9c926
jpalanco <jose.palanco@gmail.com>
2025-08-21 10:53:50 +0200

    fix: sync all version files to v0.4.2

--- src/reticulum/__init__.py
+++ src/reticulum/__init__.py
@@ -2,7 +2,7 @@
 Reticulum - Cloud Infrastructure Exposure Scanner
 """
 
-__version__ = "0.4.1"
+__version__ = "0.4.2"
 __author__ = "Jose Palanco <jose.palanco@plexicus.ai>"
 __license__ = "MIT"
 
--- src/reticulum/cli.py
+++ src/reticulum/cli.py
@@ -187,7 +187,7 @@ Examples:
         help="Display beautiful formatted output in console",
     )
 
-    parser.add_argument("--version", action="version", version="%(prog)s 0.4.1")
+    parser.add_argument("--version", action="version", version="%(prog)s 0.4.2")
 
     return parser
 
