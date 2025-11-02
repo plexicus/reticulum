150f216799458df75af36e95b5a2836a262bc86f
jpalanco <jose.palanco@gmail.com>
2025-11-02 18:50:10 +0100

    fix: sync all version files to v0.5.1

--- src/reticulum/cli.py
+++ src/reticulum/cli.py
@@ -49,7 +49,7 @@ Examples:
         help="Export network topology as Graphviz DOT file",
     )
 
-    parser.add_argument("--version", action="version", version="%(prog)s 0.6.0")
+    parser.add_argument("--version", action="version", version="%(prog)s 0.5.1")
 
     return parser
 
